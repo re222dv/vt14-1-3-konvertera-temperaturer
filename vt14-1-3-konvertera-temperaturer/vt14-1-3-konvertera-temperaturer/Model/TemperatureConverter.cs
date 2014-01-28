9fce68fefcbaa739afbc048763a07f515a169371
re222dv <re222dv@student.lnu.se>
2014-01-28 17:11:26 +0100

    Add model

---
+++ vt14-1-3-konvertera-temperaturer/vt14-1-3-konvertera-temperaturer/Model/TemperatureConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vt14_1_3_konvertera_temperaturer.Model {
+    public static class TemperatureConverter {
+
+        public int CelsiusToFahrenheit(int degreeC) {
+            return (int) Math.Round(degreeC * 1.8 + 32);
+        }
+
+        public int FahrenheitToCelsius(int degreeF) {
+            return (int) Math.Round((double) (degreeF - 32) * 5/9);
+        }
+    }
+}
